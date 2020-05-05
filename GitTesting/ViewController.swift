1cdcfe306c386a8616cce7f5bcfcc0b34453c3a1
Ben Griffith <blg.griffith@gmail.com>
2020-05-05 07:23:04 -0700

    Initial Commit

---
+++ GitTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTesting
+//
+//  Created by Ben Griffith on 05/05/20.
+//  Copyright © 2020 Ben Griffith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
